82163bf861b3401259891e6dd9ed8a67cbb731d9
Omer Eisenberg <omereis@yahoo.com>
2020-07-06 11:08:37 +0000

    updated source, make

--- psd/rp_read.c
+++ psd/rp_read.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv){
 	}
 
         /*LOOB BACK FROM OUTPUT 2 - ONLY FOR TESTING*/
-	uint32_t buff_size = 16384;//8192;//16384;
+	uint32_t buff_size = 5000;//12500;//16384;//8192;//16384;
 	float *buff = (float *)malloc(buff_size * sizeof(float));
 	float *big_buff = (float*) calloc(2 * buff_size, sizeof(big_buff[0]));
 
@@ -27,7 +27,7 @@ int main(int argc, char **argv){
 	if (rp_AcqSetSamplingRate(RP_SMP_125M) != RP_OK)
 		printf ("Setting sampleing rate error\n");
 	rp_AcqSetTriggerLevel(RP_CH_1, 10e-3); //Trig level is set in Volts while in SCPI
-	rp_AcqSetTriggerDelay(1024);
+	rp_AcqSetTriggerDelay(5000);
 
         // there is an option to select coupling when using SIGNALlab 250-12
         // rp_AcqSetAC_DC(RP_CH_1, RP_AC); // enables AC coupling on channel 1
@@ -86,3 +86,46 @@ int main(int argc, char **argv){
 
 	return 0;
 }
+//-----------------------------------------------------------------------------
+void get_options (int argc, char **argv, float *prTrigger, int *pnSamples, short *pfHelp, char **pszFile)
+{
+	int c;
+	*prTrigger = 0, *pnSamples = 0, *pfHelp = 0, *pszFile = NULL;
+
+	while ((c = getopt (argc, argv, "ht:n:f:")) != -1)
+		switch (c) {
+			case 'H':
+			case 'h':
+				*pfHelp = 1;
+				return;
+			case 't':
+			case 'T':
+				*prTrigger = atof (optarg);
+				break;
+			case 'n':
+			case 'N':
+				*pnSamples = atoi (optarg);
+				break;
+			case 'f':
+			case 'F':
+				*pszFile = optarg;
+				break;
+			default:
+				printf("Unfamiliar option: %c\n", c);
+				*pfHelp = 1;
+				return;
+		}
+} 
+//-----------------------------------------------------------------------------
+void print_usage()
+{
+	char *szMessage = "Red Pitaya RF input\n"
+					"Synopsis:\n"
+					"./read_signal -t <trigger [volts]> -n <# of samples> -f <output file name>\n"
+					"  Defaults:\n"
+					"    Trigger: 10mV:\n"
+					"    Samples: 10,000\n"
+					"    File: out.csv\n";
+	printf ("%s\n", szMessage);
+}
+//-----------------------------------------------------------------------------
