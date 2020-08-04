9bfa27c7bfaa704cab4d02766a177b3863670692
Omer Eisenberg <omereis@yahoo.com>
2020-08-04 11:50:53 +0000

    exchange buffer length with iterations

--- psd/rnhist.sh
+++ psd/rnhist.sh
@@ -1,39 +1,39 @@
-./rp_read -i 5000 -n 5000
+./rp_read -n 5000 -i 5000
 echo 'mv hist_sum.csv hist_sum_5k_05k.csv'
 mv hist_sum.csv hist_sum_5k_05k.csv
 mv sums.csv sum_5k_05k.csv
 
-./rp_read -i 5000 -n 10000
+./rp_read -n 5000 -i 10000
 echo 'mv hist_sum.csv hist_sum_5k_10k.csv'
 mv hist_sum.csv hist_sum_5k_10k.csv
 mv sums.csv sum_5k_10k.csv
 
-./rp_read -i 5000 -n 15000
+./rp_read -n 5000 -i 15000
 echo 'mv hist_sum.csv hist_sum_5k_15k.csv'
 mv hist_sum.csv hist_sum_5k_15k.csv
 mv sums.csv sum_5k_15k.csv
 
-./rp_read -i 5000 -n 20000
+./rp_read -n 5000 -i 20000
 echo 'mv hist_sum.csv hist_sum_5k_20k.csv'
 mv hist_sum.csv hist_sum_5k_20k.csv
 mv sums.csv sum_5k_20k.csv
 
-./rp_read -i 10000 -n 5000
+./rp_read -n 10000 -i 5000
 echo 'mv hist_sum.csv hist_sum_10k_05k.csv'
 mv hist_sum.csv hist_sum_10k_05k.csv
 mv sums.csv sum_5k_05k.csv
 
-./rp_read -i 10000 -n 10000
+./rp_read -n 10000 -i 10000
 echo 'mv hist_sum.csv hist_sum_10k_10k.csv'
 mv hist_sum.csv hist_sum_10k_10k.csv
 mv sums.csv sum_5k_10k.csv
 
-./rp_read -i 10000 -n 15000
+./rp_read -n 10000 -i 15000
 echo 'mv hist_sum.csv hist_sum_10k_15k.csv'
 mv hist_sum.csv hist_sum_10k_15k.csv
 mv sums.csv sum_5k_15k.csv
 
-./rp_read -i 10000 -n 20000
+./rp_read -n 10000 -i 20000
 echo 'mv hist_sum.csv hist_sum_10k_20k.csv'
 mv hist_sum.csv hist_sum_10k_20k.csv
 mv sums.csv sum_5k_20k.csv
