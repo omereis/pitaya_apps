fdb8bbb86376d1bf049424e53e36cd1af09c9a3a
Omer Eisenberg <omereis@yahoo.com>
2020-08-05 05:53:03 +0000

    script running all options

--- psd/rnhist.sh
+++ psd/rnhist.sh
@@ -1,3 +1,73 @@
+./rp_read -n 1000 -i 5000
+echo 'mv hist_sum.csv hist_sum_1k_05k.csv'
+mv hist_sum.csv hist_sum_1k_05k.csv
+mv sums.csv sum_1k_05k.csv
+
+./rp_read -n 1000 -i 10000
+echo 'mv hist_sum.csv hist_sum_1k_10k.csv'
+mv hist_sum.csv hist_sum_1k_10k.csv
+mv sums.csv sum_1k_10k.csv
+
+./rp_read -n 1000 -i 15000
+echo 'mv hist_sum.csv hist_sum_1k_15k.csv'
+mv hist_sum.csv hist_sum_1k_15k.csv
+mv sums.csv sum_1k_15k.csv
+
+./rp_read -n 1000 -i 20000
+echo 'mv hist_sum.csv hist_sum_1k_20k.csv'
+mv hist_sum.csv hist_sum_1k_20k.csv
+mv sums.csv sum_1k_20k.csv
+
+./rp_read -n 2000 -i 5000
+echo 'mv hist_sum.csv hist_sum_2k_05k.csv'
+mv hist_sum.csv hist_sum_2k_05k.csv
+mv sums.csv sum_2k_05k.csv
+
+./rp_read -n 2000 -i 10000
+echo 'mv hist_sum.csv hist_sum_2k_10k.csv'
+mv hist_sum.csv hist_sum_2k_10k.csv
+mv sums.csv sum_2k_10k.csv
+
+./rp_read -n 2000 -i 15000
+echo 'mv hist_sum.csv hist_sum_2k_15k.csv'
+mv hist_sum.csv hist_sum_2k_15k.csv
+mv sums.csv sum_2k_15k.csv
+
+./rp_read -n 2000 -i 20000
+echo 'mv hist_sum.csv hist_sum_2k_20k.csv'
+mv hist_sum.csv hist_sum_2k_20k.csv
+mv sums.csv sum_2k_20k.csv
+
+./rp_read -n 2000 -i 25000
+echo 'mv hist_sum.csv hist_sum_2k_25k.csv'
+mv hist_sum.csv hist_sum_2k_25k.csv
+mv sums.csv sum_2k_25k.csv
+
+./rp_read -n 2000 -i 30000
+echo 'mv hist_sum.csv hist_sum_2k_30k.csv'
+mv hist_sum.csv hist_sum_2k_30k.csv
+mv sums.csv sum_2k_30k.csv
+
+./rp_read -n 3000 -i 5000
+echo 'mv hist_sum.csv hist_sum_3k_05k.csv'
+mv hist_sum.csv hist_sum_3k_05k.csv
+mv sums.csv sum_3k_05k.csv
+
+./rp_read -n 3000 -i 10000
+echo 'mv hist_sum.csv hist_sum_3k_10k.csv'
+mv hist_sum.csv hist_sum_3k_10k.csv
+mv sums.csv sum_3k_10k.csv
+
+./rp_read -n 3000 -i 15000
+echo 'mv hist_sum.csv hist_sum_3k_15k.csv'
+mv hist_sum.csv hist_sum_3k_15k.csv
+mv sums.csv sum_3k_15k.csv
+
+./rp_read -n 3000 -i 20000
+echo 'mv hist_sum.csv hist_sum_3k_20k.csv'
+mv hist_sum.csv hist_sum_3k_20k.csv
+mv sums.csv sum_3k_20k.csv
+
 ./rp_read -n 5000 -i 5000
 echo 'mv hist_sum.csv hist_sum_5k_05k.csv'
 mv hist_sum.csv hist_sum_5k_05k.csv
