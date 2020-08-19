26d30d95e1edda9f6bc5b03d379ba5223139925e
Omer Eisenberg <omereis@yahoo.com>
2020-08-19 07:44:40 +0000

    deleted unsused files

--- psd/rpmk.sh
+++
@@ -1,6 +0,0 @@
-#echo 'gcc -o rp_read rp_read.c fpga_osc.c -g -Wall -Werror -I/opt/redpitaya/include -I../../api/include -L/opt/redpitaya/lib -DVERSION=0.00-0000 -DREVISION=devbuild -lm -lpthread -l:librp.so'
-#gcc -o rp_read rp_read.c fpga_osc.c -g -Wall -Werror -I/opt/redpitaya/include -I../../api/include -L/opt/redpitaya/lib -DVERSION=0.00-0000 -DREVISION=devbuild -lm -lpthread -l:librp.so
-echo 'gcc -o rp_read rp_read.c -g -Wall -Werror -I/opt/redpitaya/include -I../../api/include -L/opt/redpitaya/lib -DVERSION=0.00-0000 -DREVISION=devbuild -lm -lpthread -l:librp.so'
-gcc -o rp_read rp_read.c -g -Wall -Werror -I/opt/redpitaya/include -I../../api/include -L/opt/redpitaya/lib -DVERSION=0.00-0000 -DREVISION=devbuild -lm -lpthread -l:librp.so
-
-
--- psd/rr.sh
+++
@@ -1,4 +0,0 @@
-echo 'gcc -o fast_ai fpga_osc.o main_osc.o worker.o fast_ai.o -g -std=gnu99 -Wall -Werror -I../../api/include -DVERSION=0.0 -0000 -DREVISION=devbuild -lm -lpthread'
-
-gcc -o read_signal -g -std=gnu99 -Wall -Werror -I../../api/include -DVERSION=0.00-0000 -DREVISION=devbuild -lm -lpthread
-
