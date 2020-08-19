bf16456be1d6f8fd42cc72e5789ee0410f7a1cb7
Omer Eisenberg <omereis@yahoo.com>
2020-08-19 07:51:36 +0000

    removed file

--- psd/mk.sh
+++
@@ -1,4 +0,0 @@
-echo 'gcc -o read_signal read_signal.c -g -std=gnu99 -Wall -Werror -I../../api/include -L/opt/redpitaya/lib -DVERSION=0.00-0000 -DREVISION=devbuild -lm -lpthread -l:librp.so'
-gcc -o read_signal read_signal.c -g -std=gnu99 -Wall -Werror -I/opt/redpitaya/include -I../../api/include -L/opt/redpitaya/lib -DVERSION=0.00-0000 -DREVISION=devbuild -lm -lpthread -l:librp.so
-
-
